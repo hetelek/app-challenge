e5cbe0ead14b0d719b56126075aee43a90d84ac0
Stevie Hetelekides <hetelek@gmail.com>
2016-01-29 18:01:23 -0500

    Separate select choice/modifier GUI code

--- ios-app/ios-app/SelectScene.swift
+++ ios-app/ios-app/SelectScene.swift
@@ -42,6 +42,19 @@ class SelectScene : SKScene
         // set background color/scale
         self.scaleMode = .ResizeFill
         
+        // setup GUI based on the state
+        if self.state == .SelectChoice
+        {
+            self.steupSelectChoiceGUI()
+        }
+        else if self.state == .SelectModifier
+        {
+            self.steupSelectModifierGUI()
+        }
+    }
+    
+    private func steupSelectChoiceGUI()
+    {
         // screen center
         let centerX = CGRectGetMidX(self.frame)
         let centerY = CGRectGetMidY(self.frame)
@@ -54,35 +67,32 @@ class SelectScene : SKScene
         let horizontalSpacing: CGFloat = width + self.HORIZONTAL_SPACING * 0.75
         let verticalSpacing: CGFloat = height + self.VERTICAL_SPACING * 0.75
         
-        // setup GUI based on the state
-        if self.state == .SelectChoice
+        // get random choices
+        let choices = Game.sharedInstance.randomChoices()
+        
+        // create buttons
+        let spacing: [[CGFloat]] = [ [-1, 1], [1, 1], [-1, -1], [1, -1] ]
+        for (index, choice) in choices.enumerate()
         {
-            // get random choices
-            let choices = Game.sharedInstance.randomChoices()
+            // create start button
+            let button = CoolButton(color: UIColor.blackColor(), size: CGSize(width: width, height: height))
+            button.text = choice.choice
+            button.tag = choice
+            button.addTarget(self, selector: "optionSelected:")
             
-            // create buttons
-            let spacing: [[CGFloat]] = [ [-1, 1], [1, 1], [-1, -1], [1, -1] ]
-            for (index, choice) in choices.enumerate()
-            {
-                // create start button
-                let button = CoolButton(color: UIColor.blackColor(), size: CGSize(width: width, height: height))
-                button.text = choice.choice
-                button.tag = choice
-                button.addTarget(self, selector: "optionSelected:")
-                
-                // calculate points
-                let x = centerX + spacing[index][0] * (horizontalSpacing / 2)
-                let y = centerY + spacing[index][1] * (verticalSpacing / 2)
-                button.position = CGPoint(x: x, y: y)
-                
-                // add button to scene
-                self.addChild(button)
-            }
-        }
-        else if self.state == .SelectModifier
-        {
+            // calculate points
+            let x = centerX + spacing[index][0] * (horizontalSpacing / 2)
+            let y = centerY + spacing[index][1] * (verticalSpacing / 2)
+            button.position = CGPoint(x: x, y: y)
             
+            // add button to scene
+            self.addChild(button)
         }
+    }
+    
+    private func steupSelectModifierGUI()
+    {
+        
     }
     
     func optionSelected(button: CoolButton)
