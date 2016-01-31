38b55830a54b4c50a53dda9689ce7ea005f47a5c
Ryan Bell <RyanofRIT@users.noreply.github.com>
2016-01-31 17:31:36 -0500

    Update ConfirmStartScene.swift

--- ios-app/ios-app/ConfirmStartScene.swift
+++ ios-app/ios-app/ConfirmStartScene.swift
@@ -27,9 +27,49 @@ class ConfirmStartScene : SKScene
         // set background color/scale
         self.scaleMode = .ResizeFill
         self.backgroundColor = SKColor.gameYellowColor()
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        // screen center
-        let centerX = CGRectGetMidX(self.frame)
-        let centerY = CGRectGetMidY(self.frame)
+        // create start button
+        let startGameButton = CoolButton(color: SKColor.gameYellowColor(), size: CGSize(width: CGRectGetWidth(self.frame) + 5, height: CGRectGetHeight(self.frame) / 2 + 5))
+        startGameButton.text = "Start Game"
+        startGameButton.label.fontName = "Raleway-Bold"
+        startGameButton.flatColor = true
+        startGameButton.label.fontSize = 32
+        startGameButton.fontColor = SKColor.gameBlueColor()
+        startGameButton.addTarget(self, selector: "startGameButtonTapped:")
+        startGameButton.position = CGPoint(x: 0, y: CGRectGetHeight(self.frame) / 4)
+        
+        self.addChild(startGameButton)
+        
+        // create instructions button
+        let instructionsButton = CoolButton(color: SKColor.gameBlueColor(), size: CGSize(width: CGRectGetWidth(self.frame) + 5, height: CGRectGetHeight(self.frame) / 2 + 5))
+        instructionsButton.text = "Instructions"
+        instructionsButton.label.fontName = "Raleway-Bold"
+        instructionsButton.flatColor = true
+        instructionsButton.label.fontSize = 32
+        instructionsButton.fontColor = SKColor.gameYellowColor()
+        instructionsButton.position = CGPoint(x: 0, y: -CGRectGetHeight(self.frame) / 4)
+        instructionsButton.addTarget(self, selector: "instructionsButtonTapped:")
+        
+        self.addChild(instructionsButton)
+        
+    }
+    
+    func startGameButtonTapped(button: CoolButton)
+    {
+        // present next scene ()
+        if let confirmStartScreen = ConfirmStartScene(fileNamed: "ConfirmStartScene")
+        {
+            self.view?.presentScene(confirmStartScreen, transition: SKTransition.fadeWithDuration(0.5))
+        }
+    }
+    
+    func instructionsButtonTapped(button: CoolButton)
+    {
+        // present next scene (instructions)
+        if let instructionsScreen = InstructionsScene(fileNamed: "InstructionsScene")
+        {
+            self.view?.presentScene(instructionsScreen, transition: SKTransition.fadeWithDuration(0.5))
+        }
     }
 }
